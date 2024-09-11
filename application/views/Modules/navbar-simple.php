f36e8ae4fbf81fa2071661a100301cbe98f5d673
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-09-10 20:19:14 -0500

    Add config base

---
+++ application/views/Modules/navbar-simple.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<div class="navbar navbar-default" role="navigation">
+	<div class="navbar-header">
+		<a class="" href="<?= base_url(); ?>Index/index">
+			<span class="navbar-brand" style="height:auto; padding-left: 0px; display:flex;">
+				<img src="<?= base_url('Public'); ?>/images/logo/rocket.svg" style="width: 30px;"
+					alt="Logo de Cobranza Imagen" />
+				<img src="<?= base_url('Public'); ?>/images/logo/logo.webp" style="margin-left:5px; "
+					alt="Logo de Cobranza Letra" />
+			</span>
+		</a>
+	</div>
+</div>
