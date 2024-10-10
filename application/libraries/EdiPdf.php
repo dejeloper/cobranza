57177a47ed3fd30820cf9a4ea5cb22243386c515
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-10-10 03:52:03 -0500

    refactor: delete unused pdf libraries

--- application/libraries/EdiPdf.php
+++ application/libraries/EdiPdf.php
@@ -1,15 +1,3 @@
 <?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
-require 'tcpdf/tcpdf.php';
-
-class EdiPdf extends TCPDF {
-
-    function __construct() {
-        parent::__construct();
-        $CI = & get_instance();
-    }
-
-}
-
-?>
+defined('BASEPATH') or exit('No direct script access allowed');
--- application/libraries/Excel.php
+++ application/libraries/Excel.php
@@ -1,14 +1,9 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-require_once APPPATH . "/third_party/PHPExcel/PHPExcel.php";
 
-class Excel extends PHPExcel {
+class Excel
+{
 
-    public function __construct() {
-        parent::__construct();
-    }
-
+    public function __construct() {}
 }
-
-?>
